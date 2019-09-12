7d297d01e286c0bf5fc6dcad88a0374eb7f233be
Index197511 <hiro.saten@gmail.com>
2019-09-12 14:29:47 +0900

    implement IncrementalSearch

--- src/jump_dir/mod.rs
+++ src/jump_dir/mod.rs
@@ -3,7 +3,7 @@ extern crate sled;
 use std::path::Path;
 use sled::Db;
 
-pub fn seace_and_jump(db_path: &Path, searched_word: &str) -> Vec<(String, String)> {
+pub fn search(db_path: &Path, searched_word: &str) -> Vec<(String, String)> {
     let tree = Db::open(db_path).unwrap();
     let iter_tree = tree.iter();
     
@@ -15,6 +15,11 @@ pub fn seace_and_jump(db_path: &Path, searched_word: &str) -> Vec<(String, Strin
         let value: String = String::from_utf8(unwrapped_keyvalue.1.to_vec()).unwrap();
         fav_dir.push((key, value));
     }
-    
+    let fav_dir = fav_dir.into_iter().filter(|(_k, v)| v.contains(searched_word)).collect::<Vec<(String, String)>>();
     fav_dir
+}
+
+
+pub fn jump(dest_dir: &Path) -> () {
+    println!("{}", dest_dir.to_str().unwrap());
 }
