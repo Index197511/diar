8f04c678242b5fd546715b3f8f93249256d052d2
Index197511 <hiro.saten@gmail.com>
2019-09-12 12:06:20 +0900

    toDo Incremental Search

---
+++ src/jump_dir/mod.rs
@@ -0,0 +1,20 @@
+extern crate sled;
+
+use std::path::Path;
+use sled::Db;
+
+pub fn seace_and_jump(db_path: &Path, searched_word: &str) -> Vec<(String, String)> {
+    let tree = Db::open(db_path).unwrap();
+    let iter_tree = tree.iter();
+    
+    let mut fav_dir: Vec<(String, String)> = Vec::new();
+
+    for t in iter_tree {
+        let unwrapped_keyvalue = t.unwrap();
+        let key: String = String::from_utf8(unwrapped_keyvalue.0.to_vec()).unwrap();
+        let value: String = String::from_utf8(unwrapped_keyvalue.1.to_vec()).unwrap();
+        fav_dir.push((key, value));
+    }
+    
+    fav_dir
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 pub mod add_favorite;
 pub mod delete_favorite;
 pub mod list_favorite;
-pub mod select_then_jump;
+pub mod jump_dir; 
 pub mod types;
 pub mod util;
 #[macro_use] extern crate failure;
