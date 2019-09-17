8a39ecbc70228b392b86c69d719b4a28ca85d618
Index197511 <hiro.saten@gmail.com>
2019-09-17 14:15:24 +0900

    modify add_favorite

--- src/add_favorite/mod.rs
+++ src/add_favorite/mod.rs
@@ -5,10 +5,9 @@ use std::path::Path;
 
 pub fn add_to_db(path: &Path, key: String, db_path: &Path) -> () {
     let tree = Db::open(db_path).unwrap();
-    match tree.set(key, path.to_str().unwrap().as_bytes().to_vec()) {
-        Ok(Some(p)) => {}
-        _ => {
-            //error
-        }
+    if path.exists() {
+        tree.insert(key, path.to_str().unwrap().as_bytes().to_vec());
+    } else {
+        println!("This path does not exist!: {}", path.to_str().unwrap());
     }
 }
