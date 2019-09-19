ea94115555916957e3739f3f6edbdfe3c58dff28
Index197511 <hiro.saten@gmail.com>
2019-09-19 19:42:37 +0900

    upgrade jump

--- src/jump_dir/mod.rs
+++ src/jump_dir/mod.rs
@@ -13,16 +13,20 @@ pub fn search_and_jump(user_input: String, db_path: &Path) -> () {
         }
 
         _ => {
-           println!("Not found") 
+            let fav_dirs = search(&user_input, tree); 
+            println!("Is this what you are jumping?");
+            for t in fav_dirs {
+                println!(" key: {}, path: {}", t.0, t.1);
+            }
         }
+
     }
 
 
 }
 
 #[warn(dead_code)]
-fn search(searched_word: &str, db_path: &Path) -> Vec<(String, String)> {
-    let tree = Db::open(db_path).unwrap();
+fn search(searched_word: &str, tree: sled::Db) -> Vec<(String, String)> {
     let iter_tree = tree.iter();
     
     let mut fav_dir: Vec<(String, String)> = Vec::new();
@@ -33,7 +37,7 @@ fn search(searched_word: &str, db_path: &Path) -> Vec<(String, String)> {
         let value: String = String::from_utf8(unwrapped_keyvalue.1.to_vec()).unwrap();
         fav_dir.push((key, value));
     }
-    let fav_dir = fav_dir.into_iter().filter(|(_k, v)| v.contains(searched_word)).collect::<Vec<(String, String)>>();
+    let fav_dir = fav_dir.into_iter().filter(|(k, _v)| k.contains(searched_word)).collect::<Vec<(String, String)>>();
     fav_dir
 }
 
@@ -42,9 +46,3 @@ fn jump(dest_dir: &Path) -> () {
     println!("{}", dest_dir.to_str().unwrap());
 }
 
-#[warn(dead_code)]
-fn read<T: std::str::FromStr> () -> T {
-    let mut l = String::new();
-    std::io::stdin().read_line(&mut l).ok();
-    l.trim().parse().ok().unwrap()
-}
