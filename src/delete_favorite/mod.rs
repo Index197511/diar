b972185cb97484f31f83b822013864291aa9ce4e
4afS <g2x649@gmail.com>
2019-09-24 15:33:28 +0900

    refactor delete

--- src/delete_favorite/mod.rs
+++ src/delete_favorite/mod.rs
@@ -1,12 +1,12 @@
 use std::path::Path;
 use sled::Db;
 
+use diar::util::print_done_if_ok;
+
 pub fn delete_from_db(key: &str, db_path: &Path) {
-    let tree = Db::open(db_path).unwrap();
-    match tree.get(key) {
-        Ok(Some(_p)) => {
-            tree.remove(key);
-        }
-        _ => println!("This key does not exist!: {}", key)
+    let db = Db::open(db_path).unwrap();
+    match db.get(key) {
+        Ok(Some(_)) => print_done_if_ok(db.remove(key)),
+        _ => println!("This key does not exist!: {}", key),
     };
 }
--- src/jump_dir/mod.rs
+++ src/jump_dir/mod.rs
@@ -1,20 +1,22 @@
 use std::path::Path;
 use sled::Db;
 
+// use diar::util::print_done_if_ok;
+
 pub fn search_and_jump(user_input: String, db_path: &Path) -> () {
-    let tree = Db::open(db_path).unwrap();
-    let some_value = tree.get(&user_input);
-    match some_value {
+    let db = Db::open(db_path).unwrap();
+    let matched_value = db.get(&user_input);
+    match matched_value {
         Ok(Some(path)) => {
             let converted_str: String = String::from_utf8(path.to_vec()).unwrap();
             jump(Path::new(&converted_str));
         }
 
         _ => {
-            let fav_dirs = search(&user_input, tree); 
+            let favorites = search(&user_input, db); 
             println!("Is this what you are jumping?");
-            for t in fav_dirs {
-                println!(" key: {}, path: {}", t.0, t.1);
+            for (key, path) in favorites {
+                println!(" key: {}, path: {}", key, path);
             }
         }
 
@@ -23,18 +25,18 @@ pub fn search_and_jump(user_input: String, db_path: &Path) -> () {
 
 }
 
-fn search(searched_word: &str, tree: sled::Db) -> Vec<(String, String)> {
-    let iter_tree = tree.iter();
+fn search(searched_word: &str, db: sled::Db) -> Vec<(String, String)> {
+    let iter_tree = db.iter();
     
-    let mut fav_dir: Vec<(String, String)> = Vec::new();
+    let mut favorites: Vec<(String, String)> = Vec::new();
 
     for t in iter_tree {
         let unwrapped_keyvalue = t.unwrap();
         let key: String = String::from_utf8(unwrapped_keyvalue.0.to_vec()).unwrap();
         let value: String = String::from_utf8(unwrapped_keyvalue.1.to_vec()).unwrap();
-        fav_dir.push((key, value));
+        favorites.push((key, value));
     }
-    let fav_dir = fav_dir.into_iter().filter(|(k, _v)| k.contains(searched_word)).collect::<Vec<(String, String)>>();
+    let fav_dir = favorites.into_iter().filter(|(k, _)| k.contains(searched_word)).collect::<Vec<(String, String)>>();
     fav_dir
 }
 
