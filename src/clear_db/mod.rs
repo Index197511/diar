274253d44ad27c63e06e3e838c592941015bd1f4
4afS <g2x649@gmail.com>
2019-09-24 15:11:41 +0900

    refactor clear_db

--- src/clear_db/mod.rs
+++ src/clear_db/mod.rs
@@ -2,8 +2,8 @@ use std::path::Path;
 use sled::Db;
 
 pub fn clear_db(db_path: &Path) -> () {
-    let tree = Db::open(db_path).unwrap();
-    match tree.clear() {
+    let db = Db::open(db_path).unwrap();
+    match db.clear() {
         Ok(_) => println!("done"),
         Err(e) => println!("{}", e),
     }
