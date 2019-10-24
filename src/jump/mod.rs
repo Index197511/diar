9ab343dce9f072eb997c1ee7697bd2c7e60de390
Index197511 <hiro.saten@gmail.com>
2019-10-24 17:38:03 +0900

    define diar ls

--- src/jump/mod.rs
+++ src/jump/mod.rs
@@ -1,8 +1,7 @@
 use sled::Db;
 use std::path::Path;
 
-use diar::types::Favorite;
-use diar::util::get_favorites;
+use diar::util::{suggest, search};
 
 pub fn jump_if_matched(user_input: String, db_path: &Path) {
     let db = Db::open(db_path).unwrap();
@@ -23,19 +22,3 @@ fn jump(dest_dir: &Path) {
     println!("{}", dest_dir.to_str().unwrap());
 }
 
-fn suggest(searched: Vec<Favorite>) {
-    println!("Is this what you are jumping?");
-    for (key, path) in searched {
-        println!("       {} -> {}", key, path);
-    }
-}
-
-fn search(searched_word: &str, db: sled::Db) -> Vec<Favorite> {
-    let iter_db = db.iter();
-    let favorites = get_favorites(iter_db);
-
-    favorites
-        .into_iter()
-        .filter(|(key, _)| key.contains(searched_word))
-        .collect::<Vec<Favorite>>()
-}
