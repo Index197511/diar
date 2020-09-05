fbac363cddc0c6b37e6f5f4bb16673bc231aa39b
4afS <4afs.h1@gmail.com>
2020-09-05 22:12:10 +0900

    define constructor

--- src/domain/model.rs
+++ src/domain/model.rs
@@ -5,3 +5,12 @@ pub struct Favorite<'a> {
     pub name: entity::Name<'a>,
     pub path: &'a Path,
 }
+
+impl<'a> Favorite<'a> {
+    pub fn new(name: &'a str, path: &'a Path) -> Self {
+        Favorite {
+            name: name,
+            path: path,
+        }
+    }
+}
