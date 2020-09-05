1289e64b59eaf33a056e22dcf54166a7acdcb32e
4afS <4afs.h1@gmail.com>
2020-09-05 22:39:34 +0900

    add remove all and rename to exists

--- src/domain/repository.rs
+++ src/domain/repository.rs
@@ -4,10 +4,11 @@ use super::entity::Name;
 pub trait Repository {
   type Error;
   fn add(&self, favorite: Favorite) -> DbResponse<(), Self::Error>;
-  fn remove(&self, name: Name) -> DbResponse<(), Self::Error>;
   fn get_all(&self) -> DbResponse<Vec<Favorite>, Self::Error>;
   fn get(&self, name: Name) -> DbResponse<Favorite, Self::Error>;
-  fn exist(&self, name: Name) -> DbResponse<bool, Self::Error>;
+  fn remove(&self, name: Name) -> DbResponse<(), Self::Error>;
+  fn remove_all(&self) -> DbResponse<(), Self::Error>;
+  fn exists(&self, name: Name) -> DbResponse<bool, Self::Error>;
 }
 
 pub type DbResponse<T, E: std::fmt::Display> = Result<T, E> ;
