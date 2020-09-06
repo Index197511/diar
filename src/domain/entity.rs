422b9e92c5d793ef4c17100c3b7f543f160c4103
4afS <4afs.h1@gmail.com>
2020-09-06 12:06:08 +0900

    use struct

--- src/domain/entity.rs
+++ src/domain/entity.rs
@@ -1,1 +1,12 @@
-pub type Name<'a> = &'a str;
+// pub type Name<'a> = &'a str;
+#[macro_use]
+use derive_new::new;
+
+#[derive(new)]
+pub struct Name<'a>(&'a str);
+
+impl<'a> Name<'a> {
+  pub fn name(&self) -> &str {
+    self.0
+  }
+}
