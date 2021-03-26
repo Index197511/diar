eaac8622dfb71c6462a2e2b6b5f05d59268b78ab
4afS <4afS.h1@gmail.com>
2021-03-27 06:42:14 +0900

    update modules

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,13 @@ pub mod command;
 pub mod error;
 pub mod types;
 pub mod util;
+
+pub mod domain {
+    pub mod model;
+    pub mod repository;
+}
+
+pub mod infrastructure {
+    pub mod db;
+    pub mod repository;
+}
