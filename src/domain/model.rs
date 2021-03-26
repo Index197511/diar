d5bcac19522422adbd3c7f55e4c65696fb0c6fd0
4afS <4afS.h1@gmail.com>
2021-03-27 07:29:49 +0900

    define command model

--- src/domain/model.rs
+++ src/domain/model.rs
@@ -1,5 +1,6 @@
 use derive_new::new;
 use getset::Getters;
+use strum_macros::EnumString;
 
 #[derive(new, Getters)]
 pub struct Favorite {
@@ -7,4 +8,23 @@ pub struct Favorite {
     name: String,
     #[getset(get = "pub")]
     path: String,
+}
+
+#[derive(Debug, PartialEq, EnumString)]
+#[strum(serialize_all = "snake_case")]
+pub enum Command {
+    Add,
+    Delete,
+    Rename,
+    List,
+    Jump,
+    Clear,
+    Ls,
+}
+
+#[test]
+fn test_from_str() {
+    use std::str::FromStr;
+
+    assert_eq!(Command::from_str("add").unwrap(), Command::Add);
 }
