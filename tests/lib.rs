612fc629b42ec0c0858c9b3fbbd90a2887e24f01
4afS <4afS.h1@gmail.com>
2021-03-28 01:05:23 +0900

    update lib.rs

--- tests/lib.rs
+++ tests/lib.rs
@@ -1,3 +1,17 @@
 mod domain {
     mod model;
 }
+
+pub(self) mod infrastructure {
+    pub mod inmemory {
+        pub mod db;
+        pub mod repository;
+    }
+
+    mod repository;
+}
+
+mod commands {
+    mod add;
+    mod clear;
+}
