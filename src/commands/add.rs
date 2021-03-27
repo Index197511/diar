3f146c7b6b5b80db8f1ea4a88ff21bbfcedd5bba
4afS <4afS.h1@gmail.com>
2021-03-27 10:47:34 +0900

    return as result

--- src/commands/add.rs
+++ src/commands/add.rs
@@ -1,17 +1,17 @@
 use crate::command::WhereToAdd;
 use crate::domain::repository::IRepository;
-use crate::interface::presenter::{error, print_result};
-use crate::{
-    command::{CommandError, CommandResult},
-    domain::model::Favorite,
-};
+use crate::{command::CommandError, domain::model::Favorite};
 use std::env;
 use std::fs;
 use std::path::Path;
 
-pub fn add_favorite<T: IRepository>(repo: T, key: String, path: WhereToAdd) {
+pub fn add_favorite<T: IRepository>(
+    repo: T,
+    key: String,
+    path: WhereToAdd,
+) -> anyhow::Result<Favorite> {
     if repo.exists(&key).unwrap() {
-        return error(&CommandError::GivenKeyIsAlreadyExists.to_string());
+        return Err(CommandError::GivenKeyIsAlreadyExists.into());
     }
 
     match path {
@@ -20,29 +20,40 @@ pub fn add_favorite<T: IRepository>(repo: T, key: String, path: WhereToAdd) {
     }
 }
 
-fn add_path_to_db<T: IRepository>(repo: T, key: String, path: &Path) {
-    let path_str = path.to_str().unwrap().to_string();
+fn add_given_path_to_db<T: IRepository>(
+    repo: T,
+    key: String,
+    path: &Path,
+) -> anyhow::Result<Favorite> {
+    if !path.exists() {
+        return Err(CommandError::PathNotFound.into());
+    }
 
-    print_result(
-        repo.add(&Favorite::new(key.clone(), path_str)),
-        CommandResult::Added(key, path.to_str().unwrap().to_owned()),
+    let favorite = Favorite::new(
+        key,
+        fs::canonicalize(path)
+            .unwrap()
+            .as_path()
+            .to_str()
+            .unwrap()
+            .to_owned(),
     );
-}
 
-fn add_given_path_to_db<T: IRepository>(repo: T, key: String, path: &Path) {
-    if path.exists() {
-        add_path_to_db(repo, key, fs::canonicalize(path).unwrap().as_path());
-    } else {
-        error(&format!(
-            "This path does not exist: {}",
-            path.to_str().unwrap()
-        ));
+    match repo.add(&favorite) {
+        Ok(_) => Ok(favorite),
+        Err(e) => Err(e),
     }
 }
 
-fn add_current_path_to_db<T: IRepository>(repo: T, key: String) {
+fn add_current_path_to_db<T: IRepository>(repo: T, key: String) -> anyhow::Result<Favorite> {
     match env::current_dir() {
-        Ok(current_path) => add_path_to_db(repo, key, &current_path),
-        Err(e) => println!("{}", e),
+        Ok(current_path) => {
+            let favorite = Favorite::new(key, current_path.as_path().to_str().unwrap().to_owned());
+            match repo.add(&favorite) {
+                Ok(_) => Ok(favorite),
+                Err(e) => Err(e),
+            }
+        }
+        Err(e) => Err(e.into()),
     }
 }
