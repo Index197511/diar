2c776b8261efd0f144e6d774e40518e69fb37b65
4afS <4afS.h1@gmail.com>
2021-03-27 09:35:15 +0900

    use repository interface

--- src/commands/delete.rs
+++ src/commands/delete.rs
@@ -1,13 +1,14 @@
-use sled::Db;
-
 use crate::command::{print_result, CommandResult};
+use crate::domain::repository::IRepository;
 use crate::error::error;
 
-pub fn delete_from_db(db: Db, key: String) {
-    match db.get(&key) {
-        Ok(Some(path)) => {
-            let path_string: String = String::from_utf8(path.to_vec()).unwrap();
-            print_result(db.remove(&key), CommandResult::Deleted(key, path_string));
+pub fn delete_from_db<T: IRepository>(repo: T, key: String) {
+    match repo.get(&key) {
+        Ok(Some(favorite)) => {
+            print_result(
+                repo.remove(&key),
+                CommandResult::Deleted(key, favorite.path().clone()),
+            );
         }
         _ => error(&format!("This key does not exist!: {}", key)),
     };
