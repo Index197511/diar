efa7d3d518de8038413b90f6e534a03f484abd73
4afS <4afS.h1@gmail.com>
2021-03-28 09:13:13 +0900

    add member name

--- src/infrastructure/db.rs
+++ src/infrastructure/db.rs
@@ -3,16 +3,18 @@ use sled::Error::Io;
 use std::io::{Error, ErrorKind};
 use std::path::Path;
 
-pub struct DbHandler(pub sled::Db);
+pub struct DbHandler {
+    pub handler: sled::Db,
+}
 
 impl DbHandler {
-    pub fn new(dir: &str) -> Result<Self, sled::Error> {
-        match to_abs_path(dir) {
+    pub fn new(directory_name: &str) -> Result<Self, sled::Error> {
+        match to_abs_path(directory_name) {
             None => Err(Io(Error::new(
                 ErrorKind::NotFound,
                 "cannot found home directory",
             ))),
-            Some(path) => sled::open(Path::new(&path)).map(DbHandler),
+            Some(path) => sled::open(Path::new(&path)).map(|handler| DbHandler { handler }),
         }
     }
 }
--- src/infrastructure/repository.rs
+++ src/infrastructure/repository.rs
@@ -2,15 +2,19 @@ use super::db::DbHandler;
 use crate::domain::model::Favorite;
 use crate::domain::repository::IRepository;
 use anyhow::Result;
+use derive_new::new;
 use std::io::{Error, ErrorKind};
 
-pub struct Repository(pub DbHandler);
+#[derive(new)]
+pub struct Repository {
+    pub db: DbHandler,
+}
 
 impl IRepository for Repository {
     fn add(&self, favorite: &Favorite) -> Result<Favorite> {
         match self
-            .0
-             .0
+            .db
+            .handler
             .insert(favorite.name(), favorite.path().as_bytes().to_vec())
         {
             Ok(_) => Ok(favorite.clone()),
@@ -20,7 +24,7 @@ impl IRepository for Repository {
 
     fn get_all(&self) -> Result<Vec<Favorite>> {
         let mut favorites: Vec<Favorite> = Vec::new();
-        for favorite in self.0 .0.iter() {
+        for favorite in self.db.handler.iter() {
             if let Ok((k, v)) = favorite {
                 if let (Ok(name), Ok(path)) =
                     (String::from_utf8(k.to_vec()), String::from_utf8(v.to_vec()))
@@ -40,7 +44,7 @@ impl IRepository for Repository {
     }
 
     fn get(&self, name: &str) -> Result<Option<Favorite>> {
-        Ok(match self.0 .0.get(name)? {
+        Ok(match self.db.handler.get(name)? {
             Some(value) => Some(Favorite::new(
                 name.to_string(),
                 String::from_utf8(value.to_vec())?,
@@ -50,8 +54,8 @@ impl IRepository for Repository {
     }
 
     fn remove(&self, name: &str) -> Result<Option<Favorite>> {
-        let favorite = self.0 .0.get(name)?.and_then(|path| {
-            self.0 .0.remove(name).ok()?;
+        let favorite = self.db.handler.get(name)?.and_then(|path| {
+            self.db.handler.remove(name).ok()?;
             Some(Favorite::new(
                 name.to_string(),
                 String::from_utf8(path.to_vec()).ok()?,
@@ -62,14 +66,14 @@ impl IRepository for Repository {
     }
 
     fn remove_all(&self) -> Result<()> {
-        match self.0 .0.clear() {
+        match self.db.handler.clear() {
             Ok(_) => Ok(()),
             Err(e) => Err(e.into()),
         }
     }
 
     fn exists(&self, name: &str) -> Result<bool> {
-        match self.0 .0.get(name)? {
+        match self.db.handler.get(name)? {
             Some(_) => Ok(true),
             None => Ok(false),
         }
